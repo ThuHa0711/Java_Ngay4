4dbfdb9791fb60f4067bdb4bced773bc7cd2fa2b
ThuHa0711 <thuh62765@gmail.com>
2025-05-17 03:12:14 +0700

    update bank

--- src/main/java/com/example/Java_Ngay4/Bank.java
+++ src/main/java/com/example/Java_Ngay4/Bank.java
@@ -7,10 +7,12 @@ public class Bank {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
     private final Semaphore semaphore = new Semaphore(10);
 
+    // Thêm tài khoản mới
     public void addAccount(Account account) {
         accounts.put(account.getAccountId(), account);
     }
 
+    // Trả về tài khoản dựa trên ID
     public Account getAccount(int accountId) {
         return accounts.get(accountId);
     }
@@ -19,6 +21,7 @@ public class Bank {
         return accounts;
     }
 
+    // Giao dịch giữa 2 tài khoản
     public boolean transfer(int fromAccountId, int toAccountId, double amount) {
         Account fromAccount = accounts.get(fromAccountId);
         Account toAccount = accounts.get(toAccountId);
